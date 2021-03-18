cf8711a4aed40e017ac45c2d590a2de9b3869ffd
Rae Kolke <sheilakolke@gmail.com>
2021-03-18 23:51:01 +0000

    all updated files

--- scripts.js
+++ scripts.js
@@ -27,3 +27,37 @@ x.innerHTML = "Enjoy!";
 x.style.fontSize = '40px';
 x.style.fontFamily = 'Noto Sans JP';
 x.style.textAlign = 'center';
+
+// generic AJAX function to load fromFile into object with ID whereTo
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// prepares code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+		
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.getElementById(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+				
+			}
+		
+	} // end ajax.onreadystatechange
+
+	// now that everything is set, initiate request
+	ajax.send();
+
+}
+
+window.onload = function() {
+	loadFileInto("ingredients.html", "ingredients");
+	loadFileInto("equipment.html", "equipment");
+  loadFileInto("directions.html", "directions");
+};
+
